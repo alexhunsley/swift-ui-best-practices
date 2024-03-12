5f133f82fb0de9d5db2a8816eeb96c5b25d94974
Alex Hunsley <alex.hunsley@gmail.com>
2024-03-12 22:18:29 +0000

    WIP

--- DualMetrics/ContentView.swift
+++ DualMetrics/ContentView.swift
@@ -48,15 +48,17 @@ struct ContentView: View {
             // TODO get the index from the environment, which in turn comes from notch detection etc.
             //
 //            let m0 = MetricsSelector<CGFloat>(metrics: Metrics(), index: 0)
-            let m0 = MetricsSelector<Metrics<CGFloat>, CGFloat>(metrics: Metrics(), index: 0)
-            let lookup0 = m0(\.vertPadding)
+//            let m0 = MetricsSelector<Metrics<CGFloat>, CGFloat>(metrics: MetricsOuter.Metrics(), index: 0)
+            
+            let lookup0 = MetricsOuter().m0(\.vertPadding)
             print("Lookup: \(lookup0)")
+
 //
 //            let m1 = MetricsSelector<CGFloat>(metrics: Metrics(), index: 1)
 //            let lookup1 = m1(\.vertPadding)
 //            print("Lookup: \(lookup1)")
 
-            let mc = MetricsSelector<ColorMetrics<Color>, Color>(metrics: ColorMetrics(), index: 1)
+            let mc = MetricsSelector<MetricsOuter.ColorMetrics<Color>, Color>(metrics: MetricsOuter.ColorMetrics(), index: 1)
             let lookup2 = mc(\.textColor)
             print("Lookup: \(lookup2)")
         }
@@ -65,55 +67,27 @@ struct ContentView: View {
     // new stuff
     typealias MetricsStorage<T> = [T]
 
-//    let c = CGFloat(1.0)
+    struct MetricsOuter {
+        let m0 = MetricsSelector<Metrics<CGFloat>, CGFloat>(metrics: MetricsOuter.Metrics(), index: 0)
 
-    struct Metrics<T> {
-//        typealias T = CGFloat
+        struct Metrics<T> {
+            // without the annoation, this bit compiles, but the references
+            // to it can't see the type.
+            let horizPadding: MetricsStorage<CGFloat> = [10.0]
+            let vertPadding: MetricsStorage<CGFloat> = [20.0, 60.0]
+        }
 
-//        let horizPadding: MetricsStorage<CGFloat> = [10.0]
-//        let vertPadding: MetricsStorage<CGFloat> = [20.0, 60.0]
-
-        // without the annoation, this bit compiles, but the references
-        // to it can't see the type.
-        let horizPadding: MetricsStorage<CGFloat> = [10.0]
-        let vertPadding: MetricsStorage<CGFloat> = [20.0, 60.0]
-
-        // could have e.g. colours in here.
-        // OR a seperate metrics obj entirely.
-        // Quite like idea of in here....
-//        struct Colors {
-//            let textColor: MetricsStorage<Color> = [Color.yellow, Color.green]
-//        }
-    }
-
-    struct ColorMetrics<T> {
-//        typealias T = Color
-        let textColor: MetricsStorage<Color> = [Color.yellow, Color.green]
+        struct ColorMetrics<T> {
+            let textColor: MetricsStorage<Color> = [Color.yellow, Color.green]
+        }
     }
 
     // M = metric type (struct)
     // T = type stored (CGFloat, Color, etc)
     struct MetricsSelector<M, T> {
-//    struct MetricsSelector { //}<T> {
         let metrics: M
         let index: Int
 
-//        var data: [T]
-
-//        init(from metrics: Metrics, keyPath: KeyPath<Pair<T>, T>) {
-//        init(from metrics: Metrics, index: Int) {
-//            self.data = metrics.map { $0[index] }
-//                //$0[keyPath: keyPath] }
-//        }
-
-        // replace with the instance thing
-//        func m(_ keyPath: KeyPath<Metrics, MetricsStorage>) -> CGFloat {
-//            metrics[keyPath: keyPath][index]
-//        }
-
-//        func callAsFunction(_ keyPath: KeyPath<T, MetricsStorage<CGFloat>>) -> CGFloat {
-        // simpler with array as below? :
-//        func callAsFunction(_ keyPath: KeyPath<M, [T]>) -> T {
         func callAsFunction(_ keyPath: KeyPath<M, MetricsStorage<T>>) -> T {
             metrics[keyPath: keyPath][index]
         }
@@ -124,23 +98,3 @@ struct ContentView: View {
     ContentView()
 }
 
-
-
-// original stuff
-
-struct Pair<T> {
-    var first: T
-    var second: T
-}
-
-struct PairStorage<T> {
-    var pairs: [Pair<T>]
-}
-
-struct Echo<T> {
-    var data: [T]
-
-    init(from storage: PairStorage<T>, keyPath: KeyPath<Pair<T>, T>) {
-        self.data = storage.pairs.map { $0[keyPath: keyPath] }
-    }
-}
