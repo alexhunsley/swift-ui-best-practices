e114bd1150dea589de268e51579ef8fae9561ad0
Alex Hunsley <alex.hunsley@gmail.com>
2024-03-13 07:33:37 +0000

    WIP environment to get layout to subcomponents

--- DualMetrics/ContentView.swift
+++ DualMetrics/ContentView.swift
@@ -7,110 +7,101 @@
 
 import SwiftUI
 
+//
+// [ ] get the index from the environment, which in turn comes from notch detection etc.
+// [ ] make our own padding variant (modifier) that takes a keypath.
+//
+
 struct ContentView: View {
+    // top level View: make layout for the environment
+    // TODO get proper index
+    @StateObject var layout = Metrics(index: 0).layout
+
     var body: some View {
+        let mets = Metrics(index: 0)
+        let layout = mets.layout
+
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
                 .padding(20)
             Text("Hello, world!")
+                .padding(layout(\.vertPadding))
+
+            SubComponentView()
         }
-        .padding()
         .onAppear {
-            // original stuff
-            //            let storage = PairStorage(pairs: [Pair(first: 1, second: 2), Pair(first: 3, second: 4)])
-            //
-            //            // Echoing the 'first' property of each pair
-            //            let echoFirst = Echo(from: storage, keyPath: \.first)
-            //            print(echoFirst.data) // [1, 3]
-            //
-            //            // Echoing the 'second' property of each pair
-            //            let echoSecond = Echo(from: storage, keyPath: \.second)
-            //            print(echoSecond.data) // [2, 4]
-            //
-
-            // new stuff
-//            let mm = Metrics()
-//            print(mm)
-
-//            let k: KeyPath<Metrics<CGFloat>, MetricsStorageCGFloat> = \.horizPadding
-
-
-//            print("Keypath = \(k)")
-//            print("Value at m.keypath = \(mm[keyPath: k])")
-//
-//            let k2: KeyPath<Metrics, MetricsStorage> = \.vertPadding
-//            print("Keypath = \(k2)")
-//            print("Value at m.keypath = \(mm[keyPath: k2])")
-//
-
-            // TODO get the index from the environment, which in turn comes from notch detection etc.
-            //
-//            let m0 = MetricsSelector<CGFloat>(metrics: Metrics(), index: 0)
-//            let m0 = MetricsSelector<Metrics<CGFloat>, CGFloat>(metrics: MetricsOuter.Metrics(), index: 0)
-            
-
-//
-//            let m1 = MetricsSelector<CGFloat>(metrics: Metrics(), index: 1)
-//            let lookup1 = m1(\.vertPadding)
-//            print("Lookup: \(lookup1)")
-
-            let mc = MetricsSelector<Metrics.ColorsX, Color>(metrics: Metrics.ColorsX(), index: 0)
-            let lookup2 = mc(\.myColor)
-            print("Lookup: \(lookup2)")
-
-
-            // proper test here:
-            let mets = Metrics(index: 1)
-            let layout = mets.layout
-            let color = mets.colors2
-
             let vertPadding = layout(\.vertPadding)
             print("vertPadding: \(vertPadding)")
-
-            let textColor = color(\.myColor)
-            print("textColor: \(textColor)")
         }
+        .environmentObject(layout)
     }
 
-    // new stuff
-    typealias MetricsStorage<T> = [T]
+    // temp -- just for now
+    var isNotchedDev: Bool {
+        true
+    }
+}
+
+// example sub-component.
+// so we can test environemnt for picking up the metrics
+struct SubComponentView: View {
+
+//    @Environment(Metrics.Layout.self) var layout: MetricsSelector<Layout>
+    @EnvironmentObject var layout: MetricsSelector<Metrics.Layout>
+
+    var body: some View {
+        Text("Sub-component")
+            .padding(.horizontal, layout(\.horizPadding))
+            .background(.yellow)
+    }
+}
+
+// Metrics magic
+//extension ContentView {
+    // I've simplified to remove the metric type from the generics,
+    // as I thik CGFloat is fine. We're not likely to have e.g. different
+    // Colors for screen classes.
+    // But I suppose CGSize could be v useful (but we'd get by without, for now).
+    typealias MetricType = CGFloat
+    // The metric array is ordered as [default size, small size].
+    // This is so that the items at index 0 and 1 are always the same
+    // thing, even when we only have an array of 1 metric.
+    typealias MetricsStorage = [MetricType]
 
     struct Metrics {
-        // use typealias here? to reduce need to mention CGFloat....?
-        // TODO make these in init, which takes the index we want to use.
-        let layout: MetricsSelector<Layout, CGFloat> //(metrics: Metrics.Layout(), index: 0)
-        let colors2: MetricsSelector<ColorsX, Color> //(metrics: Metrics.ColorsX(), index: 0)
+        let layout: MetricsSelector<Layout>
 
         init(index: Int) {
             layout = .init(metrics: Metrics.Layout(), index: index)
-            colors2 = .init(metrics: Metrics.ColorsX(), index: index)
         }
 
         struct Layout {
-            // Without the annoation, this bit compiles, but the references
-            // to it can't see the type.
-            let horizPadding: MetricsStorage<CGFloat> = [10.0]
-            let vertPadding: MetricsStorage<CGFloat> = [20.0, 60.0]
-        }
-
-        struct ColorsX {
-            let myColor: MetricsStorage<Color> = [Color.yellow, Color.green]
+            let horizPadding: MetricsStorage = [100.0]
+            let vertPadding: MetricsStorage = [60.0, 20.0]
         }
     }
 
-    // M = metric type (struct)
-    // T = type stored (CGFloat, Color, etc)
-    struct MetricsSelector<M, T> {
+    // We must use a class because we have to be ObservableObject
+    // per environment requirements.
+    // This class is not inteneded to update layout dyamically, though,
+    // so we don't need any published properties
+    // M = metric type
+    class MetricsSelector<M>: ObservableObject {
         let metrics: M
         let index: Int
 
-        func callAsFunction(_ keyPath: KeyPath<M, MetricsStorage<T>>) -> T {
+        init(metrics: M, index: Int) {
+            self.metrics = metrics
+            self.index = index
+        }
+
+        func callAsFunction(_ keyPath: KeyPath<M, MetricsStorage>) -> MetricType {
             metrics[keyPath: keyPath][index]
         }
     }
-}
+//}
 
 #Preview {
     ContentView()
