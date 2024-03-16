4b2546ea04708af1f1bf42d3c86ead49107eb26f
Alex Hunsley <alex.hunsley@gmail.com>
2024-03-16 09:26:28 +0000

    Add notes WRT best practices for SwiftUI

--- DualMetrics/ContentView.swift
+++ DualMetrics/ContentView.swift
@@ -10,11 +10,20 @@ import SwiftUI
 //
 // [ ] get the index from the environment, which in turn comes from notch detection etc.
 // [ ] make our own padding variant (modifier) that takes a keypath.
-//
+// [ ] make dualMetrics more ergo: remove need to put in isLargeDesign or not (single value)? annoying!
+//         ideally not have to specify it.
+
+// if we share layout via env, fofr our own stuff, DON'T share to other folk's components!
+
+// Don't split out stringIds to diff file. It just slows us down if we have to rename or move things.
 
 // So maye the GeometryProxy can have the layout passed in to the
 
-struct ContentView: View {
+// should name indicate it's a view for full screen? hmm think not? But could be useful.
+// full screen views often have magic in them like nav, edge insets stuff etc, so I don't
+// think it's a bad thing.
+// Call this xScreenView?
+struct UserInfoView: View {
     // top level View: make layout for the environment
     // TODO get proper index
     // Probably don't need state object here at all, actually! We don't care about that.
@@ -22,7 +31,11 @@ struct ContentView: View {
 //    var layout = Metrics(index: UIDevice.current.isNotchedDevice ? 0 : 1).layout
 
     // do need stateObject, to be able to mutate!
-    @StateObject var layout: MetricsSelector<any Layout>
+    @StateObject var layout: MetricsSelector = Metrics.layout(forIndex: UIDevice.isNotchedDevice ? 0 : 1)
+
+    // VERY IMPORTANT this var qualifies what model. Do NOT use 'viewModel'. Less confusion
+    // this way, and We have to do this anyway as multiple models may in theory be in the enviroment.
+    @EnvironmentObject var userInfoViewModel: UserInfoViewModel
 
     var body: some View {
 //        let mets = Metrics(index: 0)
@@ -30,31 +43,65 @@ struct ContentView: View {
 
         // so we need to defer getting the actual layout for index until in the loopy bit.
         // TODO use geometry proxy here, and call metrics.layout(forIndex: 0/1) here based on notched?
-        let layout = Metrics.layout(forIndex: 0) // use notch!
+//        let layout = Metrics.layout(forIndex: 0) // use notch!
 
         // but how do I now set this on the environment below? Can only get hands on it in the body/geometryproxy.
         // OK, need to pass around the metrics pre-Layout call... <<<---------------------------
 
-        VStack {
+        // come back to fiddling with metrics later, you need to get most up to date code from waterfront app!
+        VStack { //}(spacing: layout(\.mainStackSpacing)) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
                 .padding(20)
-            Text("Hello, world!")
-                .padding(layout(\.vertPadding))
+
+            VStack(spacing: 20) {
+                HStack {
+                    Text("Name: \(userInfoViewModel.name)")
+                }
+                HStack {
+                    Text("Age: \(userInfoViewModel.age)")
+                }
+            }
+            .padding(layout(\.userInfoBoxPadding))
+            .border(.gray)
 
             SubComponentView()
         }
         .onAppear {
-            let vertPadding = layout(\.vertPadding)
-            print("vertPadding: \(vertPadding)")
+//            let vertPadding = layout(\.vertPadding)
+//            print("vertPadding: \(vertPadding)")
         }
         .environmentObject(layout)
     }
 
     // temp -- just for now
-    var isNotchedDev: Bool {
-        true
+//    var isNotchedDev: Bool {
+//        true
+//    }
+}
+
+extension UserInfoView {
+    // MainModel for this SwiftUI view. Or of course, the modelProvider protocol,
+    // which avoieds writing @Published on everything,
+    // but which would update on aboslutely everything
+    // (defo an issue with what we do now!) -- this is
+    // another reasonn to not use main app models directly -
+    // you possibly over-render your view -- anyhting changing
+    // in the model struct changes its value, hence a rerender.
+    //
+    // The correct strategy is for the main app to notice changes
+    // to its main model object as a whole, then update
+    // the SwiftUI model with the values. This way we don't
+    // re-render if the SwiftUI model itself doesn't change any values.
+    class UserInfoViewModel: ObservableObject {
+        @Published var name: String
+        @Published var age: Int
+
+        init(name: String, age: Int) {
+            self.name = name
+            self.age = age
+        }
     }
 }
 
@@ -62,12 +109,11 @@ struct ContentView: View {
 // so we can test environemnt for picking up the metrics
 struct SubComponentView: View {
 
-//    @Environment(Metrics.Layout.self) var layout: MetricsSelector<Layout>
     @EnvironmentObject var layout: MetricsSelector<Metrics.Layout>
 
     var body: some View {
         Text("Sub-component")
-            .padding(.horizontal, layout(\.horizPadding))
+            .padding(.horizontal, layout(\.subCompHorizPadding))
             .background(.yellow)
     }
 }
@@ -96,8 +142,11 @@ struct SubComponentView: View {
         }
 
         struct Layout {
-            let horizPadding: MetricsStorage = [100.0, 50.0]
             let vertPadding: MetricsStorage = [60.0, 20.0]
+            let userInfoBoxPadding: MetricsStorage = [60.0, 20.0]
+            // a single value used for both scren sizes
+            let mainStackSpacing = 40.0
+            let subCompHorizPadding: MetricsStorage = [100.0, 50.0]
         }
     }
 
@@ -125,16 +174,25 @@ struct SubComponentView: View {
 
 extension UIDevice {
     /// Returns `true` if the device has a notch
-    var isNotchedDevice: Bool {
+    static var isNotchedDevice: Bool {
         guard #available(iOS 11.0, *), let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first else { return false }
+//        return true
         if UIDevice.current.orientation.isPortrait {
+            print("A: \(window.safeAreaInsets.top)")
             return window.safeAreaInsets.top >= 44
         } else {
+            print("B: \(window.safeAreaInsets.left) \(window.safeAreaInsets.right)")
             return window.safeAreaInsets.left > 0 || window.safeAreaInsets.right > 0
         }
     }
 }
 
 #Preview {
-    ContentView()
+    // NB if using a main app complicated model from JSON for now,
+    // we can hydrate from a sample JSON for mock server. TODO example!
+    @ObservedObject var userInfoViewModel = UserInfoView.UserInfoViewModel(name: "Bob", age: 30)
+    // Note how the viewModel is passed in via env, not via init.
+    // This allows composition of views.
+    return UserInfoView()
+        .environmentObject(userInfoViewModel)
 }
--- DualMetrics/DualMetricsApp.swift
+++ DualMetrics/DualMetricsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DualMetricsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            UserInfoView()
         }
     }
 }
